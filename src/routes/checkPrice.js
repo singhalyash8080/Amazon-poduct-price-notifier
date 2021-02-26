14e1761506fa694da653eee5c7665d15c34e7248
Yash Singhal <yash.singhal2019@vitstudent.ac.in>
2021-02-26 23:18:45 +0530

    first commit

---
+++ src/routes/checkPrice.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const checkPrice = require('../controllers/checkPrice')
+
+const router = new express.Router()
+
+// router.post('/checkPrice',checkPrice)
+
+module.exports =  router
